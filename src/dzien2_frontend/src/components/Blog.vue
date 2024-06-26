ae25541eb297dbcabbc9f2f7c8a6fc235c38dc23
Dawideg <vaviturz@gmail.com>
2024-06-26 19:37:06 +0200

    frontend usun_wpis

--- src/dzien2_frontend/src/components/Blog.vue
+++ src/dzien2_frontend/src/components/Blog.vue
@@ -1,10 +1,20 @@
 <template>
     <div>
-        Siema
-        <button @click="pobierzWpisy">Pobierz</button>
-        {{wpisy}}
-        <input v-model="nowyBlog" type="text">
-        <button @click="dodajWpis">Dodaj</button>
+        <h2 class="text-blue-600">Wpisy na bloga</h2>
+        <div class="w-100 flex flex-row-reverse">
+            <button @click ="pobierzWpisy" class="float-right bg-blue-600 rounded text-white p-4">Refresh</button>
+        </div>
+        <div class="grid mx-6 gap-4 my-4">
+            <div v-for="(wpis,index) in wpisy" class="drop-shadow-x1 bg-stone-300 p-4">
+                <p>id: {{ index }}</p>
+                <p>{{ wpis }}</p>
+                <button @click="usun_wpis(index)">Usuń</button>
+            </div>
+        </div>
+        <div class="flex justify-center flex-col">
+            <input v-model="nowyBlog" type="text" class="border-2 border-blue-600 p-4">
+            <button @click="dodajWpis" class=" bg-blue-600 rounded text-white p-4">Dodaj</button>
+        </div>
     </div>
 </template>
 <script>
@@ -23,7 +33,13 @@ import { dzien2_backend } from 'declarations/dzien2_backend/index';
             },
             async dodajWpis(){
                 await dzien2_backend.dodaj_wpis(this.nowyBlog);
+            },
+            async usun_wpis(index){
+                await dzien2_backend.usun_wpis(index);
             }
         },
+        async mounted(){
+            this.pobierzWpisy()
+        }
     }
 </script>
